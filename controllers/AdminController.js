af7c64a715bcdf7348620b9f0d5c9874d32f9172
mohammad hosseini <mohammad@mohammads-MacBook-Pro.local>
2020-12-06 16:48:02 +0330

    some bug fixed

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const Order = require('../models/Order')
+const User = require('../models/User')
 
 router.get('/', (req,res) => {
     res.redirect('/admin/orders')
@@ -10,6 +11,11 @@ router.get('/', (req,res) => {
 router.get('/orders', async (req,res) => {
     const findOrders = await Order.find({}).populate('user')
     res.render('admin/showOrders', {orders: findOrders})
+})
+
+router.get('/users', async (req,res) => {
+    const users = await User.find()
+    res.render('admin/showUsers', {users})
 })
 
 module.exports = router
